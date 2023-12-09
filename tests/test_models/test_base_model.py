2d67735fe702ab20c2eb3fa5531b5c46e388578d
PinkNoteX <omarfarghaly8@gmail.com>
2023-12-09 06:55:13 -0800

    Tests updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
+import unittest
 from models.base_model import BaseModel
+from uuid import UUID
+from datetime import datetime
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
+class TestBaseModel(unittest.TestCase):
+    """ test basemodel """
+    my_model = BaseModel()
+
+    def testBaseModel(self):
+        """ test basemodel """
+        self.my_model.name = "My First Model"
+        self.my_model.my_number = 89
+        self.my_model.save()
+        my_model_json = self.my_model.to_dict()
+
+        self.assertEqual(self.my_model.name, my_model_json['name'])
+        self.assertEqual(self.my_model.my_number, my_model_json['my_number'])
+        self.assertEqual('BaseModel', my_model_json['__class__'])
+        self.assertEqual(self.my_model.id, my_model_json['id'])
+
+    def testSave(self):
+        """ test if update works or not """
+        self.my_model.first_name = "First"
+        self.my_model.save()
+        self.assertIsInstance(self.my_model.id, str)
+        self.assertIsInstance(self.my_model.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_model.updated_at, datetime.datetime)
+        first_dict = self.my_model.to_dict()
+        self.my_model.first_name = "Second"
+        self.my_model.save()
+        sec_dict = self.my_model.to_dict()
+        self.assertEqual(first_dict['created_at'], sec_dict['created_at'])
+        self.assertNotEqual(first_dict['updated_at'], sec_dict['updated_at'])
+
+    if __name__ == '__main__':
+        nittest.main()
